ffdb7c065289b2048f515ec6ec8ea1f71ba8bb48
PeeWee <oscar.oses.1127@gmail.com>
2020-11-28 18:38:44 -0500

    added search bar to plants component

--- client/src/pages/Plants.js
+++ client/src/pages/Plants.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "../../src/App.css";
+import { Form, Button } from 'react-bootstrap';
 
 
 
 export default function Plants() {
-    return <h1 className='Plants'>PICK YOUR PLANT</h1>;
+    return (
+        <div>
+            <h1 className='Plants'>PICK YOUR PLANT</h1>
+        <Form>
+            <Form.Group controlId="formPlantSearch">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="input" placeholder="Common Name" />
+                <Form.Text className="text-muted">
+                    Search for a specific plant using its common name.
+      </Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+    </Button>
+        </Form>
+        </div>
+    )
 } 
--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,22 @@ router.get("/plants", function (req, res) {
     })
 })
 
+router.get("/search:commonName", function (req, res) {
+  const param = req.params
+
+  const newParam = param.commonName.replaceAll(" ", "%20")
+
+  console.log(newParam);
+
+  axios.get(`https://trefle.io/api/v1/plants?token=Vfe4EOlm7UFekDua6gadyrwdwQBMC-Y_bLQEUkREGjMN&filter[common_name]=${newParam}`)
+    .then(function (response) {
+      console.log(response.data);
+      res.status(200).json(response)
+    })
+})
+
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-
-
-
-module.exports = router;
