f768a65a6680b80c2d58dbcbc7a2b1dd0592a2e6
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-11-28 12:55:45 -0500

    working on signup form

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,21 +5,6 @@ import '../Footer/footer.css';
 function Footer() {
   return (
     <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-         Sign into your account
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-          </form>
-        </div>
-      </section>
       <div className='footer-links'>
         <div className='footer-link-wrapper'>
           <div className='footer-link-items'>
