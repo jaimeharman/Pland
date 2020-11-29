476417631e55fb584833e604c8fa287311c9f10c
PeeWee <oscar.oses.1127@gmail.com>
2020-11-29 12:23:20 -0500

    changes commited

--- client/src/pages/Cultivate.js
+++ client/src/pages/Cultivate.js
@@ -5,12 +5,14 @@ import ReactWeather from "../components/ReactWeather/ReactWeather"
 
 
 export default function Cultivate() {
-    return (<ReactWeather
+    return (
+        <div className = "container">
+            <ReactWeather
         forecast="5days"
         apikey="d2dc4b5a67f8f43f9ff13956727536e2"
         type="city"
         city="Miami"
-        lang="en"
-    />
+        lang="en" />
+    </div>
     )
 } 
