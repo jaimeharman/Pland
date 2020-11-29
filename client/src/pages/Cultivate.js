1b997ff1999196fbeb8cb0ea405bdaf3a7dbd750
PeeWee <oscar.oses.1127@gmail.com>
2020-11-28 20:09:06 -0500

    fixed weather with danny's help

--- client/src/pages/Cultivate.js
+++ client/src/pages/Cultivate.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "../../src/App.css";
+import ReactWeather from "../components/ReactWeather/ReactWeather"
 
 
 
 export default function Cultivate() {
-    return <h1 className='Products'>Cultivate</h1>;
+    return (<ReactWeather
+        forecast="5days"
+        apikey="d2dc4b5a67f8f43f9ff13956727536e2"
+        type="city"
+        city="Miami"
+        lang="en"
+    />
+    )
 } 
--- models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    Plant: require("./plant")
-};
