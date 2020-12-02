4746c321d504e6b752731f50e2f5e762455bbc1e
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-12-01 19:04:58 -0500

    fixed picture bug on signup

--- client/src/components/Form/FormSuccess.jsx
+++ client/src/components/Form/FormSuccess.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './form.css';
 
+
 const FormSuccess = () => {
   return (
     <div className='form-content-right'>
       <h1 className='form-success'>We have received your request!</h1>
-      <img className='form-img-2' src='images/smelltheroses.jpg' alt='success-image' />
     </div>
   );
 };
