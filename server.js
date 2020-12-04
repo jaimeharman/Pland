b70ce7ffc46e40a10589e80ed12f779b4e830d47
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-12-03 20:43:29 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -12,10 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use("/Pland/", express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+})
+  
 }
 app.use(cors());
 app.use(morgan("dev"))
+
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 // Add routes, both API and view
 app.use(routes);
