b4d301aaf34925d50c15bec23f6eb26194467fa4
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-12-04 21:25:53 -0500

    updated server.js

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(cors());
 app.use(morgan("dev"))
 
-// app.use(express.static(path.join(__dirname, "client", "build")))
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "client", "build")))
+// app.use(express.static(path.join(__dirname, "public")));
 
 // Add routes, both API and view
 app.use(routes);
